2a3db28fe631a706a6be64dfa71f46e4d754f80e
kdten <kolten.rasmussen@gmail.com>
2023-07-01 10:13:29 -0600

    capitals

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,7 @@ export { default as Header } from "./Header/Header";
 export { default as LastAdded } from "./content/LastAdded";
 export { default as ListElement } from "./content/ListElement";
 export { default as LoaderScreen } from "./LoaderScreen/LoaderScreen";
-export { default as Playbar } from "./Playbar/Playbar";
+export { default as PlayBar } from "./Playbar/PlayBar";
 export { default as ProgressRange } from "./Playbar/ProgressRange/ProgressRange";
 export { default as FullCover } from "./Playbar/FullCover";
 export { default as VolumeIcon } from "./Playbar/VolumeIcon";
